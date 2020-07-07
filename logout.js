eb17718393e3c9aec1fd894407e19a0540c10c06
noor2020-eng <67733645+noor2020-eng@users.noreply.github.com>
2020-07-07 13:26:58 +0300

    first commit

--- logout.js
+++ logout.js
@@ -5,6 +5,9 @@ var logOut=document.getElementById("logOut");
 
 logOut.addEventListener('click',function(e){
     e.preventDefault()
+        localStorage.clear(); //but clear>>remove all item. 
+        sessionStorage.clear();
+
     location.href="index.html";
 }); 
 
